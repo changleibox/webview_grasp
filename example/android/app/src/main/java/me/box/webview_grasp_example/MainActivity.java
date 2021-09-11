6da667344ca17ed6e1c9a29bed8558369b368fb9
lei.chang <540739522@qq.com>
2021-09-11 17:19:25 +0800

    Initial commit

---
+++ example/android/app/src/main/java/me/box/webview_grasp_example/MainActivity.java
@@ -0,0 +1,6 @@
+package me.box.webview_grasp_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ ios/Classes/WebviewGraspPlugin.m
@@ -0,0 +1,20 @@
+#import "WebviewGraspPlugin.h"
+
+@implementation WebviewGraspPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"webview_grasp"
+            binaryMessenger:[registrar messenger]];
+  WebviewGraspPlugin* instance = [[WebviewGraspPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
