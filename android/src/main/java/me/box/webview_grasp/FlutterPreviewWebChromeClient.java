d1c424882e534004bb4fb932c67b1acd108c11f4
lei.chang <540739522@qq.com>
2021-09-12 12:16:43 +0800

    feat: 初始提交

--- android/src/main/java/me/box/webview_grasp/FlutterPreviewWebChromeClient.java
+++ android/src/main/java/me/box/webview_grasp/FlutterPreviewWebChromeClient.java
@@ -59,7 +59,7 @@ public class FlutterPreviewWebChromeClient extends FlutterWebChromeClient implem
         return (ViewGroup) activity.getWindow().getDecorView();
     }
 
-    private int getFullscreenSystemUiVisibility() {
+    private static int getFullscreenSystemUiVisibility() {
         return View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 // Set the content to appear under the system bars so that the
                 // content doesn't resize when the system bars hide and show.
